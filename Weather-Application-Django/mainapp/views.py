3f0a70b7706f0bf598b5777ba1d2337da34effa9
Abhijith KR <abhijithkr7745@gmail.com>
2023-10-16 20:09:34 +0530

    Weather-Application-Django - Added

---
+++ Weather-Application-Django/mainapp/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+
+import requests #for sending request
+
+# Create your views here.
+
+
+def index(request):
+
+    # Using Weather Api - https://www.weatherapi.com/
+    # 1. Sign up for a free account at [weatherapi.com](https://www.weatherapi.com/), log in, and generate your new API key in the dashboard section.
+    BASE_URL ='http://api.weatherapi.com/v1'
+
+    # 2. After generating your API key, copy it and then paste it into the "API_KEY" variable as given below:
+    API_KEY = 'paste-your-api-key' # ***************
+
+    if request.method=='POST':
+        city=request.POST.get('city').lower()
+        print(city)
+
+        if API_KEY == 'paste-your-api-key':
+            print('Please add your generated API key into the "API_KEY" variable within the views.py')
+            return render(request,'index.html',{'checker':'Please add your generated API key into the "API_KEY" variable within the views.py'})
+
+        elif (city== ''):
+            print('No value')
+            return render(request,'index.html',{'checker':'Please enter valid info...!'})
+
+        else:
+            request_url = f"{BASE_URL}/current.json?key={API_KEY}&q={city}&aqi=no"  #checking city with API (Using Weather Api)
+
+            # print(request_url)
+
+            response = requests.get(request_url)
+
+            if response.status_code == 200:
+                data = response.json()
+                # print(data)
+
+                location=data['location']
+                weather = data['current']
+                # print(weather)
+                print(location['tz_id'])
+                print(weather['temp_c'])
+
+
+                context = {
+                    'weather':weather['temp_c'],
+                    'city_name':location['name'],
+                    'region':location['region'],
+                    'country':location['country'],
+                    'lat':location['lat'],
+                    'lon':location['lon'],
+                    'localtime':location['localtime'],
+                    'continent':location['tz_id'],
+                    'static_city':city
+                }
+
+                return render(request,'index.html', context)
+
+            else:
+                print("An error occurred")
+                return render(request,'index.html',{'static_city':city,'checker':'Please enter valid city'})
+
+
+    return render(request,'index.html',{})
---
+++ Weather-Application-Django/readme.md
@@ -0,0 +1,72 @@
+# Django Weather Application
+
+Here's a simple weather application built with Python (Django). You're welcome to customize it according to your needs.
+
+
+## Pre-Requisites:
+
+1. Install the latest version of Python [ https://www.python.org/downloads/ ]
+
+2. Install the latest version of Django either globally or within a dedicated virtual environment
+```bash
+pip install django
+```
+
+3. Install Requests Library
+```bash
+pip install requests
+```
+
+4. Sign up for a free account at [weatherapi.com](https://www.weatherapi.com/), log in, and generate your new API key in the dashboard section.
+
+5. After generating your API key, copy it and then paste it into the "API_KEY" variable within the views.py file using this format:
+```bash
+API_KEY = 'paste-your-api-key'
+```
+
+## How to run this project?
+
+**Clone this project**
+```
+$  git clone https://github.com/ianshulx/Django-Projects-for-beginners
+```
+
+**Then, Enter the project directory**
+```
+$  cd Weather-Application-Django
+```
+
+**Activate Virtual Environment, if you don't have an environment, please don't follow this steps.**
+
+For Windows
+```
+$  source venv/scripts/activate
+```
+
+For Linux and Mac
+```
+$  source venv/bin/activate
+```
+
+**Install Requirements from 'requirements.txt'**
+```python
+$  pip install -r requirements.txt
+```
+
+**6. Now Run Server**
+
+Command for Windows:
+```python
+$ python manage.py runserver
+```
+
+Command for Linux or Mac:
+```python
+$ python3 manage.py runserver
+```
+
+
+## Connect with me
+
+- Github [https://github.com/abhi7745]
+- Linkedin [www.linkedin.com/in/abhijithkr-in]
