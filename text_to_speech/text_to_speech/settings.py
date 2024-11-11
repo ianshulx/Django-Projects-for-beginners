db82b9d8ffb3e28c79cc3497c2f3fb51729d6c35
Ouko Okowa <oukopinto@gmail.com>
2024-11-11 12:11:48 +0100

    Added .env file to store environment variables & gitignore to prevent .env from being pushed to GitHub

--- text_to_speech/text_to_speech/settings.py
+++ text_to_speech/text_to_speech/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1av7%kyih)8&%w1v5g^f$+(a$^f&p28y-pr!wns!@&5axfwg)2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
