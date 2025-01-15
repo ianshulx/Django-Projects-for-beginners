924c72a1ad3df4ec3f5ccde5213bb9255ed62711
karanDevX11 <bistakaran89@gmail.com>
2025-01-15 22:28:30 +0545

    Update username properly in profile

--- facebook_clone/social/settings.py
+++ facebook_clone/social/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # Third Party Packages
     'daphne',
-
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
