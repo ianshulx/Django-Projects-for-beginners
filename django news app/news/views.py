93e72195b867f08429674741a00266ac51a792d4
vishwas <vishwasferwani2002@gmail.com>
2025-02-28 12:35:18 +0530

    removed my personal api key

--- django news app/news/views.py
+++ django news app/news/views.py
@@ -7,9 +7,9 @@ def home(request):
     #for news search
     if request.method == "POST":
         search_query = request.POST.get("search_term") 
-        news_api_request=requests.get(f"https://newsapi.org/v2/everything?q={search_query}&apiKey=535b8edca7c54c5f9d01f5496d3b7a08")
+        news_api_request=requests.get(f"https://newsapi.org/v2/everything?q={search_query}&apiKey=(your_api_key)")
     else:
-        news_api_request=requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=535b8edca7c54c5f9d01f5496d3b7a08")
+        news_api_request=requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=(your_api_key)")
     api=json.loads(news_api_request.content)
     return render(request,'index.html',{'api':api})
 
