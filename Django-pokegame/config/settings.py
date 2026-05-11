001076415465b9f1a3aad6da7c150b3600946b85
Sparsh Rastogi <sparshh.sp26@gmail.com>
2026-05-12 03:30:28 +0530

    render deployment prep

--- Django-pokegame/config/settings.py
+++ Django-pokegame/config/settings.py
@@ -50,6 +50,7 @@ LOGIN_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,7 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# The directory where collectstatic will gather all files for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# This tells WhiteNoise to handle the files and even compress them for speed
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
