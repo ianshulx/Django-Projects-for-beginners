8c43903eed8e5218675fade904e0ab9e47ef977a
sania khan <sania.takhan@gmail.com>
2026-06-12 11:30:25 +0530

    Load course and session choices dynamically in student forms

--- Django Intermediate Projects/Student management system/student_management_app/forms.py
+++ Django Intermediate Projects/Student management system/student_management_app/forms.py
@@ -15,40 +15,50 @@ class AddStudentForm(forms.Form):
     username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
     address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
 
-    #For Displaying Courses
-    try:
-        courses = Courses.objects.all()
-        course_list = []
-        for course in courses:
-            single_course = (course.id, course.course_name)
-            course_list.append(single_course)
-    except:
-        course_list = []
-    
-    #For Displaying Session Years
-    try:
-        session_years = SessionYearModel.objects.all()
-        session_year_list = []
-        for session_year in session_years:
-            single_session_year = (session_year.id, str(session_year.session_start_year)+" to "+str(session_year.session_end_year))
-            session_year_list.append(single_session_year)
-            
-    except:
-        session_year_list = []
-    
     gender_list = (
         ('Male','Male'),
         ('Female','Female')
     )
-    
-    course_id = forms.ChoiceField(label="Course", choices=course_list, widget=forms.Select(attrs={"class":"form-control"}))
-    gender = forms.ChoiceField(label="Gender", choices=gender_list, widget=forms.Select(attrs={"class":"form-control"}))
-    session_year_id = forms.ChoiceField(label="Session Year", choices=session_year_list, widget=forms.Select(attrs={"class":"form-control"}))
-    # session_start_year = forms.DateField(label="Session Start", widget=DateInput(attrs={"class":"form-control"}))
-    # session_end_year = forms.DateField(label="Session End", widget=DateInput(attrs={"class":"form-control"}))
-    profile_pic = forms.FileField(label="Profile Pic", required=False, widget=forms.FileInput(attrs={"class":"form-control"}))
 
+    course_id = forms.ChoiceField(
+        label="Course",
+        choices=[],
+        widget=forms.Select(attrs={"class":"form-control"})
+    )
 
+    gender = forms.ChoiceField(
+        label="Gender",
+        choices=gender_list,
+        widget=forms.Select(attrs={"class":"form-control"})
+    )
+
+    session_year_id = forms.ChoiceField(
+        label="Session Year",
+        choices=[],
+        widget=forms.Select(attrs={"class":"form-control"})
+    )
+
+    profile_pic = forms.FileField(
+        label="Profile Pic",
+        required=False,
+        widget=forms.FileInput(attrs={"class":"form-control"})
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['course_id'].choices = [
+            (course.id, course.course_name)
+            for course in Courses.objects.all()
+        ]
+
+        self.fields['session_year_id'].choices = [
+            (
+                session.id,
+                f"{session.session_start_year} to {session.session_end_year}"
+            )
+            for session in SessionYearModel.objects.all()
+        ]
 
 class EditStudentForm(forms.Form):
     email = forms.EmailField(label="Email", max_length=50, widget=forms.EmailInput(attrs={"class":"form-control"}))
@@ -57,36 +67,47 @@ class EditStudentForm(forms.Form):
     username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
     address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
 
-    #For Displaying Courses
-    try:
-        courses = Courses.objects.all()
-        course_list = []
-        for course in courses:
-            single_course = (course.id, course.course_name)
-            course_list.append(single_course)
-    except:
-        course_list = []
-
-    #For Displaying Session Years
-    try:
-        session_years = SessionYearModel.objects.all()
-        session_year_list = []
-        for session_year in session_years:
-            single_session_year = (session_year.id, str(session_year.session_start_year)+" to "+str(session_year.session_end_year))
-            session_year_list.append(single_session_year)
-            
-    except:
-        session_year_list = []
-
-    
     gender_list = (
         ('Male','Male'),
         ('Female','Female')
     )
-    
-    course_id = forms.ChoiceField(label="Course", choices=course_list, widget=forms.Select(attrs={"class":"form-control"}))
-    gender = forms.ChoiceField(label="Gender", choices=gender_list, widget=forms.Select(attrs={"class":"form-control"}))
-    session_year_id = forms.ChoiceField(label="Session Year", choices=session_year_list, widget=forms.Select(attrs={"class":"form-control"}))
-    # session_start_year = forms.DateField(label="Session Start", widget=DateInput(attrs={"class":"form-control"}))
-    # session_end_year = forms.DateField(label="Session End", widget=DateInput(attrs={"class":"form-control"}))
-    profile_pic = forms.FileField(label="Profile Pic", required=False, widget=forms.FileInput(attrs={"class":"form-control"}))
+
+    course_id = forms.ChoiceField(
+        label="Course",
+        choices=[],
+        widget=forms.Select(attrs={"class":"form-control"})
+    )
+
+    gender = forms.ChoiceField(
+        label="Gender",
+        choices=gender_list,
+        widget=forms.Select(attrs={"class":"form-control"})
+    )
+
+    session_year_id = forms.ChoiceField(
+        label="Session Year",
+        choices=[],
+        widget=forms.Select(attrs={"class":"form-control"})
+    )
+
+    profile_pic = forms.FileField(
+        label="Profile Pic",
+        required=False,
+        widget=forms.FileInput(attrs={"class":"form-control"})
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['course_id'].choices = [
+            (course.id, course.course_name)
+            for course in Courses.objects.all()
+        ]
+
+        self.fields['session_year_id'].choices = [
+            (
+                session.id,
+                f"{session.session_start_year} to {session.session_end_year}"
+            )
+            for session in SessionYearModel.objects.all()
+        ]
